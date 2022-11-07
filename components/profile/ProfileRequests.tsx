cf1ded7b42ba6ce29ea96537c2af1bf21337d247
Hyeonjun <sty110357@gmail.com>
2022-11-07 23:57:24 +0900

    [FIX] lesson request page removed

--- components/profile/ProfileRequests.tsx
+++ components/profile/ProfileRequests.tsx
@@ -1,4 +1,4 @@
-import SongsContainer, { IData } from '@/containers/songs/SongContainer';
+import SongContainer, { IData } from '@/containers/songs/SongContainer';
 import styles from '@/styles/profile/ProfileRequest.module.scss';
 import Image from 'next/image';
 import { MdArrowForwardIos } from 'react-icons/md';
@@ -14,22 +14,23 @@ export default function MyPageRequest({ className, data }: Props) {
       {data.map((v,k) => {
         const song = [ v ];
         return (
-          <div className={styles.wrapper} key={k}>
-            <SongsContainer className={styles.song} data={song} />
-            <div className={styles.teacher}>
-              <div className={styles.img}>
-                {/* <Image /> */}
-              </div>
-              <div className={styles.name}>
-                김은수
-              </div>
-              <div className={styles.status}>
-                신청중
-              </div>
+          <></>
+          // <div className={styles.wrapper} key={k}>
+          //   <SongContainer className={styles.song} data={song} />
+          //   <div className={styles.teacher}>
+          //     <div className={styles.img}>
+          //       {/* <Image /> */}
+          //     </div>
+          //     <div className={styles.name}>
+          //       김은수
+          //     </div>
+          //     <div className={styles.status}>
+          //       신청중
+          //     </div>
 
-              <MdArrowForwardIos />
-            </div>
-          </div>
+          //     <MdArrowForwardIos />
+          //   </div>
+          // </div>
         )
       })}
     </div>

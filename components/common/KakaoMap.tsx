cdadb842a11720e92b54099b45cfbc4f43e71e4e
Hyeonjun <sty110357@gmail.com>
2022-11-29 14:14:35 +0900

    [UPDATE] kakao map error alert

--- components/common/KakaoMap.tsx
+++ components/common/KakaoMap.tsx
@@ -12,14 +12,15 @@ export default function KakaoMap({ loadMap, dong, isError }: Props) {
 
   useEffect(() => {
     loadMap(containerRef);
+    alert(isError)
   }, [loadMap]);
 
   return (
     <>
-    <div className={`${styles.map} ${isError ? styles.error:''}`} ref={containerRef} />
-    <div className={`${styles.sub} ${isError ? styles.error:''}`}>
-      {isError ? '현재 위치를 가져오는 데 문제가 발생했습니다. 위치 권한을 확인하시고 다시 시도하시거나 고객센터로 문의해주시기 바랍니다.':dong}
-    </div>
+      <div className={`${styles.map} ${isError ? styles.error:''}`} ref={containerRef} />
+      <div className={`${styles.sub} ${isError ? styles.error:''}`}>
+        {isError ? '현재 위치를 가져오는 데 문제가 발생했습니다. 위치 권한을 확인하시고 다시 시도하시거나 고객센터로 문의해주시기 바랍니다.':dong}
+      </div>
     </>
   )
 }
